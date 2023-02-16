039e4eed17562e156e671e75c84d2424f4b3d988
tj topicz <tjtopicz@gmail.com>
2023-02-16 11:20:05 -0600

    initial commit

--- helloServer.js
+++ helloServer.js
@@ -1,1 +1,14 @@
 //Comments:
+'use strict';
+
+const http = require('http');
+const port = process.env.PORT || 8000;
+
+const server = http.createServer(function(req, res) {
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello world');
+});
+
+server.listen(port, function() {
+  console.log('Listening on port', port);
+});
